ac01888e51e1cfac974de5ad5a1939b627164f17
Philip Sugg <pbensugg@gmail.com>
2016-04-26 16:22:01 -0500

    add new node modules, models for admin and user to app.js

--- app.js
+++ app.js
@@ -10,6 +10,33 @@ var users = require('./routes/users');
 
 var app = express();
 
+//my edits- to DB
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/blogwiki');
+var db = mongoose.connection;
+
+var Schema = mongoose.Schema;
+	var Page = new Schema({
+		title: String,
+		url: {type: String, index: {unique:true}},
+		content: String,
+		menuIndex: Number,
+		date: Date	
+	})
+	var Page = mongoose.model('Page', Page);
+	module.exports=Page;
+
+//The models for Page and Admin
+var adminUser = new Schema({
+	username: String,
+	password: String
+});
+var adminUser = mongoose.model('adminUser', adminUser);
+
+module.exports = adminUser;
+
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
