1b21853351ce41f1b410c17fc40ac2e9696f636e
Philip Sugg <pbensugg@gmail.com>
2016-04-26 17:15:56 -0500

    modify index route, add basic route for pages

--- app.js
+++ app.js
@@ -24,8 +24,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//routes
 app.use('/', routes);
-app.use('/users', users);
+app.use('/pages', routes);
+//app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/api.js
+++ routes/api.js
@@ -4,5 +4,20 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 var Page = require('../models.page.js')
+var adminUser = require('../models/admin-users.js');
 
+//User routes
 
+router.get('/pages', function(request, response){
+	
+	return Page.find(function(err, pages){
+		if (!err){
+			return response.send(pages);
+		}
+		else{
+			return response.send(500, err);
+		}
+	});
+})
+
+module.exports = router;
