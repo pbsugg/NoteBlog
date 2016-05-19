c8ae53719bc8f78379b942f8245328ab0b2e1eb4
Philip Sugg <pbensugg@gmail.com>
2016-05-19 17:25:18 -0500

    add basic html skeleton to application component, modify imported components to application.ts

--- app_client/app/components/application/application.ts
+++ app_client/app/components/application/application.ts
@@ -1,31 +1,28 @@
 import {Component} from "@angular/core"
-import HeaderComponent from "app/components/header/header";
 import NavbarComponent from "app/components/navbar/navbar"; 
-import LandingPageComponent from "app/components/landing-page/landing-page";
-import ActivityBarComponent from "app/components/activity-bar/activity-bar";
-import NoteComponent  from "app/components/note/note";
-import PostComponent from "app/components/post/post";
-
-
+import FooterComponent from "app/components/footer/footer";
+import LandingPageComponent  from "app/components/landing-page/landing-page";
+import SidebarComponent from "app/components/sidebar/sidebar";
 
 @Component({
 	selector: 'noteblog-application',
 	providers: [
 		Note,
 		Post
-	],
+	], 
 	templateURL: 'app/components/application/application.html',
 	styleURLs: ['app/components/application/application.css'],
 	directives: [
-		HeaderComponent,
 		NavbarComponent,
+		FooterComponent,
 		LandingPageComponent,
-		ActivityBarComponent,
-		NoteComponent,
-		PostComponent
+		SidebarComponent	
 	]
 })
 
+//not sure if I will need to import "note" and "post" components, revisit this
+//when necessary...
+//
 export default class ApplicationComponent{
 	
 	notes: Array<Note> = [];
