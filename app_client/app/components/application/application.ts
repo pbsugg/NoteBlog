6af27a8fb1beec8d112215cddcd837c9ddc7887f
Philip Sugg <pbensugg@gmail.com>
2016-05-19 16:01:28 -0500

    add components .ts file, basic config.js file

---
+++ app_client/app/components/application/application.ts
@@ -0,0 +1,22 @@
+import {Component} from "@angular/core"
+import HeaderComponent from "app/components/header/header";
+import NavbarComponent from "app/components/navbar/navbar"; 
+import LandingPageComponent from "app/components/landing-page/landing-page";
+import ActivityBarComponent from "app/components/activity-bar/activity-bar";
+import NoteComponent  from "app/components/note/note";
+import PostComponent from "app/components/post/post";
+
+
+export default class ApplicationComponent{
+	
+	notes: Array<Note> = [];
+	posts: Array<Post> = [];
+
+	constructor(notes: Note, posts: Post) {
+		//TD: write function to get five most recent notes and posts
+		this.notes = this.notes.getMostRecentNotes(5);
+		this.posts = this.posts.getMostRecentPosts(5);
+	}
+}
+
+
